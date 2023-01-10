104724c08381b63f2fec7f5ee009330740249bd7
Shosei-Imazeki <mon.sho.de@gmail.com>
2023-01-10 21:05:04 +0900

    Spring Boot3にバージョンアップ

--- src/main/java/com/example/studyspringbatch/batchprocessing/BatchConfiguration.java
+++ src/main/java/com/example/studyspringbatch/batchprocessing/BatchConfiguration.java
@@ -3,10 +3,9 @@ package com.example.studyspringbatch.batchprocessing;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
 import org.springframework.batch.item.database.JdbcBatchItemWriter;
@@ -14,22 +13,16 @@ import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilde
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
 
 @Configuration
-@EnableBatchProcessing
+//@EnableBatchProcessing
 public class BatchConfiguration {
-
-    @Autowired
-    public JobBuilderFactory jobBuilderFactory;
-
-    @Autowired
-    public StepBuilderFactory stepBuilderFactory;
 
     /* input */
     // 指定したcsvファイルのデータを各行ごとにPersonへ変換
@@ -66,8 +59,12 @@ public class BatchConfiguration {
 
     /* actual job configuration */
     @Bean
-    public Job importUserJob(JobCompletionNotificationListener listener, Step step1) {
-        return jobBuilderFactory.get("importUserJob")
+    public Job importUserJob(
+            JobRepository jobRepository,
+            JobCompletionNotificationListener listener,
+            Step step1
+    ) {
+        return new JobBuilder("importUserJob", jobRepository)
             .incrementer(new RunIdIncrementer()) // ジョブが実行状態を維持するのにDBを使用するためincrementerが必要になる
             .listener(listener)
             .flow(step1)
@@ -76,9 +73,13 @@ public class BatchConfiguration {
     }
 
     @Bean
-    public Step step1(JdbcBatchItemWriter<Person> writer) {
-        return stepBuilderFactory.get("step1")
-                .<Person, Person> chunk(10) // 一度に書き込む量を定義
+    public Step step1(
+            JobRepository jobRepository,
+            PlatformTransactionManager transactionManager,
+            JdbcBatchItemWriter<Person> writer
+    ) {
+        return new StepBuilder("step1", jobRepository)
+                .<Person, Person> chunk(10, transactionManager) // 一度に書き込む量を定義
                 .reader((reader()))
                 .processor(processor())
                 .writer(writer)
@@ -87,28 +88,5 @@ public class BatchConfiguration {
         // chunk()に<Person, Person>というprefixがついている理由
         // chunk()がジェネリックメソッドであるため。(メソッドの引数をメソッド呼び出しの際に決められるメソッドのこと)
         // ここでは、inputの引数としてPerson type, outputの引数としてPerson typeを定義している
-    }
-
-    /**
-     * バッチ2(コマンドラインから指定して動作できるかのテスト用)
-     */
-    @Bean
-    public Job testJob(JobCompletionNotificationListener listener, Step step1) {
-        return jobBuilderFactory.get("testJob")
-                .incrementer(new RunIdIncrementer()) // ジョブが実行状態を維持するのにDBを使用するためincrementerが必要になる
-                .listener(listener)
-                .flow(step1)
-                .end()
-                .build();
-    }
-
-    @Bean
-    public Step testJobStep1(JdbcBatchItemWriter<Person> writer) {
-        return stepBuilderFactory.get("step1")
-                .<Person, Person> chunk(10) // 一度に書き込む量を定義
-                .reader((reader()))
-                .processor(processor())
-                .writer(writer)
-                .build();
     }
 }
